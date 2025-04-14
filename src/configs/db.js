3a8f7ab28c229265c3b087142b83723008f4f487
dirtdk <95955656+dirtdk@users.noreply.github.com>
2025-04-14 19:26:30 +0530

    project added

---
+++ src/configs/db.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+  if (!process.env.MONGO_URI) {
+    console.error(
+      "Error: MONGO_URI is not defined in the environment variables."
+    );
+    process.exit(1); // Exit the process with failure
+  }
+
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB connected successfully.");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error.message);
+    process.exit(1); // Exit the process with failure
+  }
+};
---
+++ src/index.js
@@ -0,0 +1,44 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import { connectDB } from "./configs/db.js";
+import authRoutes from "./routes/auth_routes.js";
+import messageRoutes from "./routes/message_routes.js";
+import { app, server } from "./configs/socket.js";
+import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config({ path: path.join(__dirname, ".env") });
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "https://chat-frontend-one-omega.vercel.app/",
+    credentials: true,
+  })
+);
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+const PORT = process.env.PORT || 8081;
+
+
+// Error-handling middleware
+app.use((err, req, res, next) => {
+  console.error("Error:", err.message);
+  if (err instanceof AggregateError) {
+    console.error("AggregateError cause:", err.cause);
+  }
+  res.status(500).json({ error: "Internal Server Error" });
+});
+
+server.listen(PORT, () => {
+  console.log("server is running on PORT:" + PORT);
+  connectDB();
+});
